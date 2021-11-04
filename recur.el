46d213633d5325113e857b1c212f2b2937cf29d5
ROCKTAKEY <rocktakey@gmail.com>
2021-11-05 00:28:48 +0900

    Update version.

--- recur.el
+++ recur.el
@@ -5,7 +5,7 @@
 ;; Author: ROCKTAKEY <rocktakey@gmail.com>
 ;; Keywords: lisp
 
-;; Version: 0.0.0
+;; Version: 0.1.0
 ;; Package-Requires: ((emacs "24.3"))
 ;; URL: https://github.com/ROCKTAKEY/recur
 ;; This program is free software; you can redistribute it and/or modify
