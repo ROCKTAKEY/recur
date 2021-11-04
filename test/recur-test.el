b2ece5e23a2d46c5469eff29120a54785bc3f0d9
ROCKTAKEY <rocktakey@gmail.com>
2021-11-04 15:32:20 +0900

    Initial commit

---
+++ test/recur-test.el
@@ -0,0 +1,40 @@
+;;; recur-test.el --- test for recur
+
+;; Copyright (C) 2021  ROCKTAKEY
+
+;; Author: ROCKTAKEY <rocktakey@gmail.com>
+;; Keywords:
+
+;; Version: 0.0.0
+;; Package-Requires:
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(require 'ert)
+
+(require 'undercover)
+(undercover "*.el"
+            (:report-format 'codecov)
+            (:report-file "coverage-final.json")
+            (:send-report nil))
+
+
+
+(provide 'recur-test)
+;;; recur-test.el ends here
